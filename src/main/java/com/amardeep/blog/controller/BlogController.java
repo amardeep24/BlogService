ab1f3192d8ffdd5441935086b1deee42cab27e0b
Amardeep Bhowmick <amardeepbhowmick992@gmail.com>
2017-09-18 00:17:29 +0530

    Mock service added

--- src/main/java/com/amardeep/blog/controller/BlogController.java
+++ src/main/java/com/amardeep/blog/controller/BlogController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.amardeep.blog.service.BlogService;
  * @author AMARDEEP
  *
  */
+@CrossOrigin
 @RestController
 public class BlogController {
 	
@@ -33,16 +35,47 @@ public class BlogController {
 	@Autowired
 	BlogService blogService;
 	
+	//For dev
+	BlogDto blogDummy=new BlogDto();
+	BlogDto blogDummyTwo=new BlogDto();
+	BlogDto blogDummyThree=new BlogDto();
+	
+	BlogController(){
+	blogDummy.setBlogId(1000L);
+	blogDummy.setBlogText("John Doe");
+	blogDummy.setBlogTitle("Hello John!");
+	
+	
+	blogDummyTwo.setBlogId(2000L);
+	blogDummyTwo.setBlogText("Jane Doe");
+	blogDummyTwo.setBlogTitle("Hello Jane!");
+	
+	blogDummyThree.setBlogId(3000L);
+	blogDummyThree.setBlogText("Joe Doe");
+	blogDummyThree.setBlogTitle("This note was taken by Joe!");
+	}
+	//For dev
+	
+	@GetMapping(value="blog/",produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<BlogDto[]> getAllBlogs(){
+		logger.info("####getAllBlogs invoked####");
+		return new ResponseEntity<>(new BlogDto[]{blogDummy,blogDummyTwo,blogDummyThree},HttpStatus.OK);
+		
+	}
+	
 	@GetMapping(value="blog/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<BlogDto> getBlog(@PathVariable("id") Long id){
 		logger.info("####getBlog invoked with id####",id);
-		BlogDto blogDummy=new BlogDto();
-		blogDummy.setBlogId(1000L);
-		blogDummy.setBlogText("John Doe");
-		blogDummy.setBlogTitle("Hello John!");
-		return new ResponseEntity<>(blogDummy,HttpStatus.OK);
+		if(id==1000L){
+			return new ResponseEntity<>(blogDummy,HttpStatus.OK);
+		}else if(id==2000L){
+			return new ResponseEntity<>(blogDummyTwo,HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(blogDummyThree,HttpStatus.OK);
+		}
 		
 	}
+	
 	@PostMapping(value="blog/", consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
 	public BlogDto createBlog(BlogDto blog){
 		logger.info("####createBlog invoked with data####", blog);
